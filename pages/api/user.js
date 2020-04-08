a55cbb6f7747eae61c894543c47c8aa2cb6b655e
ELK75 <elk75@pitt.edu>
2020-04-08 13:49:09 -0400

    add get requests for users

--- pages/api/user.js
+++ pages/api/user.js
@@ -4,24 +4,29 @@ const MongoClient = require('mongodb').MongoClient;
 const dbUrl = require('../../config/index').dbUrl;
 
 
-let connectToDB = async(dbName) => {
+let connectToDB = (dbName) => {
     // Use connect method to connect to the server
     return new Promise((resolve, reject) => {
         MongoClient.connect(dbUrl, function (err, client) {
-            if (err !== null) { console.log(err); }
+            if (err !== null) { reject(err); }
             else {
                 console.log("Connected successfully to server");
             }
-            return resolve(client.db(dbName));
+            resolve(client.db(dbName));
         });
     })
 }
 
 export default async(req, res) => {
-    connectToDB('cooki').then(db => {
-        const users = db.collection('user');
+    let db = await connectToDB('cooki');
+    const users = db.collection('user');
 
+    if (req.method === 'POST') {
         users.insertMany([{ a: 1 }, { b: 2 }]);
         res.json({ message: 'success' });
-    });
+    } else {
+        users.find({ 'a': 1 }).toArray(function (err, docs) {
+            res.json(docs);
+        });
+    }
 }
